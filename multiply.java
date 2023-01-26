d3199a74a7ea0a1f0bfdd5273417bd33f788a84f
garv <garvarora778@gmail.com>
2023-01-26 16:32:37 +0530

    added my name

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package learngit;
+
+public class multiply {
+    public static void main(String args[]) {
+        int a=25;
+        int b=36;
+        System.out.println("multiply" + (a*b));
+        System.out.println("garv");
+    }
+}
