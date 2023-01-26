a56ca959a5751848be9ec274c5b9dc52c5b6680d
garv <garvarora778@gmail.com>
2023-01-26 16:58:21 +0530

    minus

--- diff.java
+++ diff.java
@@ -3,6 +3,7 @@ package learngit;
 public class diff {
     public static void main(String args[]) {
         int a=22;
-        
+        int b =90;
+        System.out.println("diff is",a-b);
     }
 }
